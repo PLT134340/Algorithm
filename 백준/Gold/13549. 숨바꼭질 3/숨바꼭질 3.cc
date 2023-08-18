4bc0442a10f208a1999be3f134af99cc8a725418
PLT134340 <108577117+PLT134340@users.noreply.github.com>
2023-08-18 16:13:51 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2348 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+bool isVisited[200001] = {false};
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    queue<pair<int, int>> q; // {pos, lv}
+    q.push({n, 0});
+    while(!q.empty()) {
+        auto [pos, lv] = q.front();
+        q.pop();
+
+        if(pos == k) {
+            cout << lv << '\n';
+            return 0;
+        }
+
+        if(2 * pos <= 200000 && !isVisited[2 * pos]) {
+            q.push({2 * pos, lv});
+            isVisited[2 * pos] = true;
+        }
+        if(pos > 0 && !isVisited[pos - 1]) {
+            q.push({pos - 1, lv + 1});
+            isVisited[pos - 1] = true;
+        }
+        if(pos < 100000 && !isVisited[pos + 1]) {
+            q.push({pos + 1, lv + 1});
+            isVisited[pos + 1] = true;
+        }
+        
+    }
+    return 0;
+}
