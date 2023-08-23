c9fc2a1e407b4ab08075610052beb0ef0424972d
PLT134340 <108577117+PLT134340@users.noreply.github.com>
2023-08-23 15:25:58 +0900

    [Silver V] Title: 숫자 카드, Time: 500 ms, Memory: 25520 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+    
+    set<int> s;
+    for(int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        s.insert(num);
+    }
+
+    int m;
+    cin >> m;
+    for(int i = 0; i < m; i++) {
+        int num;
+        cin >> num;
+        if(s.find(num) != s.end())
+            cout << 1 << ' ';
+        else
+            cout << 0 << ' ';
+    }
+    return 0;
+}
