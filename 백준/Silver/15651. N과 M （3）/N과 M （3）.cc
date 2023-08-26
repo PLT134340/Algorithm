2d93dd86b0527a6152bfb3728037b03b85abbdfb
PLT134340 <108577117+PLT134340@users.noreply.github.com>
2023-08-27 01:33:05 +0900

    [Silver III] Title: N과 M (3), Time: 704 ms, Memory: 61012 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    queue<vector<int>> q;
+    for(int i = 1; i <= n; i++) {
+        vector<int> v;
+        v.push_back(i);
+        q.push(v);
+    }
+    
+    while(!q.empty()) {
+        auto v = q.front();
+        q.pop();
+
+        if(v.size() == m) {
+            for(int i = 0; i < v.size(); i++)
+                cout << v[i] << ' ';
+            cout << '\n';
+            continue;
+        }
+        
+        for(int i = 1; i <= n; i++) {
+            auto newV = v;
+            newV.push_back(i);
+            q.push(newV);
+        }
+    }
+    return 0;
+}
