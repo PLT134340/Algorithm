0e91402f0e7dac4c374d45d40492bd866009aa8f
PLT134340 <108577117+PLT134340@users.noreply.github.com>
2023-08-29 11:00:40 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 168 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+// #include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> v;
+    for(int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+
+        if(v.empty() || v.back() < num)
+            v.push_back(num);
+        else
+            *lower_bound(v.begin(), v.end(), num) = num;
+    }
+    
+    cout << v.size() << '\n';
+    return 0;
+}
