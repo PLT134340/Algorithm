b67c93cbdf18fa464bc8112d4bac7317d4291025
김창휘 <108577117+kch1015@users.noreply.github.com>
2024-04-04 18:32:17 +0900

    [Bronze V] Title: 사파리월드, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        long m = scanner.nextLong();
+        
+        System.out.println(Math.abs(n - m));
+    }
+}
