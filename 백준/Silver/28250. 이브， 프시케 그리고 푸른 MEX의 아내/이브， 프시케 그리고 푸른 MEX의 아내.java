dc75a4ba19151d9334aba2ef59d9019114adfa65
김창휘 <108577117+kch1015@users.noreply.github.com>
2024-04-23 20:57:47 +0900

    [Silver II] Title: 이브, 프시케 그리고 푸른 MEX의 아내, Time: 340 ms, Memory: 34896 KB -BaekjoonHub

--- 백준/Silver/28250. 이브， 프시케 그리고 푸른 MEX의 아내/이브， 프시케 그리고 푸른 MEX의 아내.java
+++ 백준/Silver/28250. 이브， 프시케 그리고 푸른 MEX의 아내/이브， 프시케 그리고 푸른 MEX의 아내.java
@@ -1,25 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+
         int[] arr = new int[3];
+        String[] line = bufferedReader.readLine().split(" ");
         for (int i = 0; i < n; i++) {
-            int tmp = scanner.nextInt();
-            if (tmp == 0)
+            if (Integer.parseInt(line[i]) == 0)
                 arr[0]++;
-            else if (tmp == 1)
+            else if (Integer.parseInt(line[i]) == 1)
                 arr[1]++;
             else
                 arr[2]++;
         }
-            
+
         long result = 0;
-        result += (long)arr[0] * (arr[0] - 1) / 2;
-        result += (long)arr[0] * arr[1] * 2;
-        result += (long)arr[0] * arr[2];
+        result += (long) arr[0] * (arr[0] - 1) / 2;
+        result += (long) arr[0] * arr[1] * 2;
+        result += (long) arr[0] * arr[2];
         System.out.println(result);
     }
 }
