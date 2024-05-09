a78fd24cbd521ba4fa1e06b4e22e39c6df4145c1
김창휘 <108577117+kch1015@users.noreply.github.com>
2024-05-09 16:34:41 +0900

    [Silver V] Title: 불사조, Time: 220 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Silver/31780. 불사조/불사조.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int x = scanner.nextInt();
+        int m = scanner.nextInt();
+        
+        System.out.println(x * (m + 1));
+    }   
+}
