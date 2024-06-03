9da7f68c8032c682e1b8d27cbd653b0b1010f5f7
김창휘 <108577117+kch1015@users.noreply.github.com>
2024-06-03 19:54:27 +0900

    [Gold V] Title: 탑, Time: 648 ms, Memory: 121636 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,36 +1,27 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
+import java.io.*;
 
 class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-
-        List<Integer> heights = Arrays.asList(br.readLine().split(" "))
-                .stream()
-                .map(s -> Integer.parseInt(s))
-                .collect(Collectors.toList());
+        
+        String[] line = br.readLine().split(" ");
+        int heights[] = new int[n];
+        for (int i = 0; i < n; i++)
+            heights[i] = Integer.parseInt(line[i]);
 
         int[] result = new int[n];
         Deque<Node> dq = new ArrayDeque<>();
 
         for (int i = n - 1; i >= 0; i--) {
-            while (!dq.isEmpty() && heights.get(i) > dq.peekLast().num) {
+            while (!dq.isEmpty() && heights[i] > dq.peekLast().num) {
                 Node nd = dq.pollLast();
                 result[nd.idx] = i + 1;
             }
-            dq.offerLast(new Node(i, heights.get(i)));
+            dq.offerLast(new Node(i, heights[i]));
         }
 
         for (int i = 0; i < n; i++)
@@ -39,14 +30,14 @@ class Main {
         bw.flush();
         bw.close();
     }
-}
-
-class Node {
-    public int idx;
-    public int num;
-
-    public Node(int idx, int num) {
-        this.idx = idx;
-        this.num = num;
-    }
+    
+    static class Node {
+        public int idx;
+        public int num;
+    
+        public Node(int idx, int num) {
+            this.idx = idx;
+            this.num = num;
+        }
+    }    
 }
