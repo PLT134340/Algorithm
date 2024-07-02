52d4bf801a12f8fa3d631dc8aa04f03a3ce031c6
김창휘 <108577117+kch1015@users.noreply.github.com>
2024-07-02 23:06:44 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 168 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        
+        boolean[] arr = new boolean[31];
+        for (int i = 0; i < 28; i++) {
+            int n = scanner.nextInt();
+            arr[n] = true;
+        }
+        
+        for (int i = 1; i < 31; i++)
+            if (!arr[i])
+                System.out.println(i);
+    }
+}
