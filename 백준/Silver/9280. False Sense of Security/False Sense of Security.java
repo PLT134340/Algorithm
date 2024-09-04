a8296d090d52eeb5ebb55fcb1556b0562c0cdb53
김창휘 <108577117+kch1015@users.noreply.github.com>
2024-09-04 21:37:58 +0900

    [Silver IV] Title: False Sense of Security, Time: 304 ms, Memory: 49284 KB -BaekjoonHub

---
+++ 백준/Silver/9280. False Sense of Security/False Sense of Security.java
@@ -0,0 +1,105 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String encryptedCode = "";
+
+        while ((encryptedCode = br.readLine()) != null) {
+            int[] length = new int[encryptedCode.length()];
+            StringBuilder middleText = new StringBuilder();
+
+            for (int i = 0; i < encryptedCode.length(); i++) {
+                Moss moss = Moss.findByCharacter(encryptedCode.charAt(i));
+
+                if (moss == null)
+                    throw new RuntimeException();
+
+                length[i] = moss.getCode().length();
+                middleText.append(moss.getCode());
+            }
+
+            StringBuilder plaintext = new StringBuilder();
+            int prevIdx = 0;
+
+            for (int i = length.length - 1; i >= 0; i--) {
+                String decryptedCode = middleText.substring(prevIdx, prevIdx + length[i]);
+                Moss moss = Moss.findByCode(decryptedCode);
+
+                if (moss == null)
+                    throw new RuntimeException();
+
+                plaintext.append(moss.character);
+                prevIdx += length[i];
+            }
+
+            System.out.println(plaintext);
+        }
+    }
+
+    public enum Moss {
+        A('A', ".-"),
+        B('B', "-..."),
+        C('C', "-.-."),
+        D('D', "-.."),
+        E('E', "."),
+        F('F', "..-."),
+        G('G', "--."),
+        H('H', "...."),
+        I('I', ".."),
+        J('J', ".---"),
+        K('K', "-.-"),
+        L('L', ".-.."),
+        M('M', "--"),
+        N('N', "-."),
+        O('O', "---"),
+        P('P', ".--."),
+        Q('Q', "--.-"),
+        R('R', ".-."),
+        S('S', "..."),
+        T('T', "-"),
+        U('U', "..-"),
+        V('V', "...-"),
+        W('W', ".--"),
+        X('X', "-..-"),
+        Y('Y', "-.--"),
+        Z('Z', "--.."),
+        UNDERSCORE('_', "..--"),
+        COMMA(',', ".-.-"),
+        PERIOD('.', "---."),
+        QUESTIONMARK('?', "----");
+
+        private final char character;
+        private final String code;
+
+        Moss(char character, String code) {
+            this.character = character;
+            this.code = code;
+        }
+
+        public static Moss findByCharacter(char character) {
+            for (Moss moss : values())
+                if (moss.character == character)
+                    return moss;
+            return null;
+        }
+
+        public static Moss findByCode(String code) {
+            for (Moss moss : values())
+                if (moss.code.equals(code))
+                    return moss;
+            return null;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String getCode() {
+            return code;
+        }
+    }
+}
