bbb52cb04f9fdce73c91e0d53898be66856349bd
김창휘 <108577117+kch1015@users.noreply.github.com>
2024-09-08 22:57:53 +0900

    [Bronze IV] Title: 영수증, Time: 180 ms, Memory: 17944 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            sum += a * b;
+        }
+
+        if (x == sum)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
