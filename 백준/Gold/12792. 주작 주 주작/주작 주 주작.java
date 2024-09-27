da97727cc5c99d8a7e4a94eb64d36f59359d258a
김창휘 <108577117+kch1015@users.noreply.github.com>
2024-09-27 13:55:38 +0900

    [Gold V] Title: 주작 주 주작, Time: 412 ms, Memory: 110428 KB -BaekjoonHub

--- 백준/Gold/12792. 주작 주 주작/주작 주 주작.java
+++ 백준/Gold/12792. 주작 주 주작/주작 주 주작.java
@@ -1,10 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 class Main {
-    static final int size = 1_100_000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -20,19 +18,6 @@ class Main {
             }
         }
 
-        boolean[] isPrime = new boolean[size + 1];
-        Arrays.fill(isPrime, true);
-        isPrime[0] = isPrime[1] = false;
-
-        for (int i = 2; i * i <= size; i++)
-            if (isPrime[i])
-                for (int j = 2; i * j <= size; j++)
-                    isPrime[i * j] = false;
-
-        for (int i = 1_000_001; i <= size; i++)
-            if (isPrime[i]) {
-                System.out.println(i);
-                return;
-            }
+        System.out.println(1_000_003);
     }
 }
